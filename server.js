385a9d9dbe458f60e1b608fd61ca2115f1c737b5
Oren <ogolan@yp.com>
2013-06-29 17:30:44 -0700

    parsing the body - using contact-stream

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var concat = require('concat-stream');
+var qs = require('querystring');
 
 http.createServer(function (req, res) {
-  if (req.url === '/') {
+  if (req.url === '/' && req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
     return;
@@ -17,6 +19,12 @@ http.createServer(function (req, res) {
   if (req.url === '/login' && req.method === 'POST') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var html = '<form method="post"> <input name="username"> <input type="password" name="password"></form>';
+
+    parseBody(req, function (body) {
+      console.dir(body);
+    });
+
+
     res.end(html);
     return;
   }
@@ -26,3 +34,9 @@ http.createServer(function (req, res) {
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000/');
+
+function parseBody(req, cb) {
+  req.pipe(concat(function (body) { 
+    cb(qs.parse(body.toString())); 
+  }));
+};
